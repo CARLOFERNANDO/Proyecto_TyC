8106774824e285f0a7cc2e33ea8f10fe49fe6d07
Jorge CardenasG <jcardenasg32@example.com>
2018-09-25 17:20:39 -0500

    delete-parte de modulo

--- app/models/Modulo.php
+++ app/models/Modulo.php
@@ -7,13 +7,11 @@
         }
         function agregarModulos($datos){
             // Modulo(_,_,_,_,_) son todos o solo unos cuantos?
-            $this->bd->query("INSERT INTO Modulo VALUES (:v_nom_mod,:v_op_ocu_mod,:v_fech_in,:v_fech_fi,:v_durac)");
+            $this->bd->query("INSERT INTO Modulo(nombreMod,duracion) VALUES (:v_nom_mod,:v_durac)");
 
             //vincular valores
             $this->bd->bind(":v_nom_mod",$datos["n_mod"]); // estos indices son iguales al 
-            $this->bd->bind(":v_op_ocu_mod",$datos["op_ocup_mod"]);//definido en el archivo pagina
-            $this->bd->bind(":v_fech_in",$datos["fech_ini_mod"]);
-            $this->bd->bind(":v_fech_fi",$datos["fech_fin_mod"]);
+            
             $this->bd->bind(":v_durac",$datos["durac_mod"]);
             if($this->bd->execute()){
                 return true;
