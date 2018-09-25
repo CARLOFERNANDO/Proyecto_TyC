097c25a7ffd6faca3a39115b46a01c986ca6e6f8
Jorge CardenasG <jcardenasg32@example.com>
2018-09-25 10:35:56 -0500

    feature-controlador modulo

---
+++ app/controlers/C_Modulo.php
@@ -0,0 +1,55 @@
+<?php
+    
+    /**
+     * EL CONTROLADORACTUAL POR DEFECTO SI NO EXISTE UN CONTROLADOR PASADO POR URL ES PAGINAS
+     * EL METODOACTUAL POR DEFECTO SI NO EXISTE UN METODO PASADO POR URL ES INDEX
+     */
+    
+    class C_Modulo extends Controlador{
+        function __construct(){
+            $this->moduloModelo=$this->modelo("Modulo");
+        }
+
+        function index(){
+            $lsmodulos=$this->moduloModelo->obtenerModulos();
+            //$datos=["titulo"=>"CONTENIDO DE UNA PAGINA EN ESPECIFICO"                                
+            //        ];
+            $datos=["modulos"=>$lsmodulos];
+            $this->vistas("paginas/modulos_mostrar",$datos); //FALTA LA PAGINA PARA MOSTRAR LOS MODULOS Y EL INGRESADO
+        }
+
+        function agregar(){
+            if($_SERVER["REQUEST_METHOD"]=="POST"){
+                $datos= [
+                    "n_mod"=> trim($_POST["nombre"]),
+                    "op_ocup_mod"=> trim($_POST["opcion-ocupacional"]),
+                    //"fech_ini_mod"=> trim($_POST["fecha-inicio"]),
+                    //"fech_fin_mod"=> trim($_POST["fecha-fin"]),
+                    "durac_mod"=> trim($_POST["duracion"])
+                ];
+                //se ejecuta el agregado y t direcciona a las paginas
+                if($this->moduloModelo->agregarModulos($datos)){
+                    redireccionar("C_Modulo"); 
+                    /**
+                     * Al principio es paginas , pero se necesita direccionar asi mismo para que
+                     * ingrese al index y lo direccione a vista
+                     */
+                }else{
+                    die ("ALGO ANDA MAL");
+                }
+            }else{ // por get y los datos seran vaciois
+                $datos = [
+                    "n_nombre"=>" ",
+                    "n_profe"=>" "
+                ];
+                //cambiamos paginas/agregar
+                $this->vistas("paginas/modulos",$datos);
+            }
+        }
+
+
+    }
+
+
+
+?>
